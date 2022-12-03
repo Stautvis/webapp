b1d39dae59bca76eff6f15a5bc84468ebeb0f2c8
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-03 22:47:22 +0200

    Login page done

--- client/src/routes/login/+page.server.ts
+++ client/src/routes/login/+page.server.ts
@@ -1,15 +1,33 @@
-import { invalid, type Actions } from '@sveltejs/kit';
-import { z } from 'zod';
+import { invalid, redirect, type Actions } from '@sveltejs/kit';
+import { z, ZodError } from 'zod';
+import { post } from '../../lib/utils/api';
 import { validateData } from '../../lib/utils/validateData';
 
 const loginScheme = z.object({
 	emailAddress: z.string({ required_error: 'Email address is required!' }).email(),
-	password: z.string({ required_error: 'Password is required!' })
+	password: z
+		.string({ required_error: 'Password is required!' })
+		.min(1, { message: 'Password is required!' })
 });
 
 export const actions: Actions = {
-	default: async ({ request }) => {
+	default: async ({ request, cookies }) => {
 		const { data, errors } = await validateData(request.formData(), loginScheme);
-		return invalid(300, { data, errors });
+
+		if (errors !== null) return invalid(300, { data, errors });
+
+		try {
+			const token = await post('/user/login', data);
+			cookies.set('Bearer', token as string);
+			console.log(cookies.get('Bearer'));
+		} catch (err) {
+			if (err instanceof ZodError) {
+				return invalid(300, {
+					data,
+					errors: err.flatten().fieldErrors
+				});
+			}
+		}
+		throw redirect(301, '/');
 	}
 };
--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,27 +1,25 @@
 <script>
 	import Button from '../../lib/components/Button.svelte';
-	/** @type {import('./$types').ActionData} */
-	export let form;
-
-	console.log(form);
-
 	import Form from '../../lib/components/Form.svelte';
 	import Input from '../../lib/components/Input.svelte';
+
+	/** @type {import('./$types').ActionData} */
+	export let form;
 </script>
 
 <Form method="post">
 	<Input
 		id="emailAddress"
 		label="Your email address"
-		helper="This is helper"
 		errors={form?.errors?.emailAddress}
-		value={form?.data?.emailAddress}
+		value={form?.data?.emailAddress || 'admin@example.com'}
 	/>
 	<Input
 		id="password"
+		type="password"
 		label="Your password"
 		errors={form?.errors?.password}
-		value={form?.data?.password}
+		value={form?.data?.password || 'password'}
 	/>
 
 	<Button>Login</Button>
