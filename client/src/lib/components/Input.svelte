bab38e5225c6ea3c352255953d8f50c1173f561e
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-03 22:48:09 +0200

    input component update

--- client/src/lib/components/Input.svelte
+++ client/src/lib/components/Input.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	type InputTypes = 'text' | 'password' | 'textarea' | 'number';
+	type InputTypes = 'text' | 'password' | 'textarea' | 'number' | 'select' | 'date';
 
 	export let type: InputTypes = 'text';
 	export let id: HTMLInputElement['id'];
 	export let placeholder: HTMLInputElement['placeholder'] = '';
-	export let value: HTMLInputElement['value'] | FormDataEntryValue = '';
+	export let value: HTMLInputElement['value'] | number = '';
 	export let label: string = '';
 	export let helper: string = '';
 	export let errors: string[] = [];
@@ -12,6 +12,12 @@
 	let wasFocued: boolean = false;
 
 	const onFocus = () => (wasFocued = true);
+
+	const getType = () => {
+		if (type == 'textarea') return type;
+		if (type == 'select') return type;
+		return 'input';
+	};
 
 	$: isErrors = errors.length !== 0;
 </script>
@@ -21,16 +27,31 @@
 		<label for={id}>{label}</label>
 	{/if}
 
-	<svelte:element
-		this={type === 'textarea' ? 'textarea' : 'input'}
-		{type}
-		{id}
-		{placeholder}
-		{value}
-		name={id}
-		on:focus={onFocus}
-		class={isErrors && !wasFocued ? 'input-error' : ''}
-	/>
+	{#if type === 'select'}
+		<svelte:element
+			this={getType()}
+			{type}
+			{id}
+			{placeholder}
+			{value}
+			name={id}
+			on:focus={onFocus}
+			class={isErrors && !wasFocued ? 'input-error' : ''}
+		>
+			<slot />
+		</svelte:element>
+	{:else}
+		<svelte:element
+			this={getType()}
+			{type}
+			{id}
+			{placeholder}
+			{value}
+			name={id}
+			on:focus={onFocus}
+			class={isErrors && !wasFocued ? 'input-error' : ''}
+		/>
+	{/if}
 
 	{#if helper !== '' || isErrors}
 		<p class={`mt-2 text-sm ${isErrors ? 'text-error' : 'text-default'}`}>
@@ -44,8 +65,13 @@
 		@apply block mb-2 text-sm font-medium text-gray-900 dark:text-white;
 	}
 
-	input {
-		@apply bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500;
+	input,
+	select {
+		@apply appearance-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500;
+	}
+
+	input[type='date'] {
+		@apply p-1.5 my-auto;
 	}
 
 	.input-error {
