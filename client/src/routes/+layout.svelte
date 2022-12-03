70884de8ddd547014cc423b780d34efbb863bcfd
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-04 01:27:17 +0200

    sidebar layout

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script>
 	import '../app.css';
+	import Sidebar from '../lib/components/Sidebar/Sidebar.svelte';
+	import SidebarItem from '../lib/components/Sidebar/SidebarItem.svelte';
 </script>
 
-<slot />
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
+	/>
+</svelte:head>
+
+<div class="flex min-h-screen h-full">
+	<Sidebar>
+		<SidebarItem title="Dashboard" href="/dashboard" icon="bi-grid-fill" />
+		<SidebarItem title="Services" href="/dashboard/service" icon="bi-badge-ad-fill">
+			<SidebarItem title="Approved" href="/dashboard/service?status=approved" />
+			<SidebarItem title="Add service" href="/dashboard/service/add" />
+		</SidebarItem>
+		<SidebarItem title="Members" href="/dashboard/member" icon="bi-person-fill">
+			<SidebarItem title="Add member" href="/dashboard/member/add" />
+		</SidebarItem>
+	</Sidebar>
+	<main class="p-10">
+		<slot />
+	</main>
+</div>
