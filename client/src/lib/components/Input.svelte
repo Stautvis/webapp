57f09753175dc86741bf4fa63472da35ccf254b3
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-04 15:04:29 +0200

    Input float number

--- client/src/lib/components/Input.svelte
+++ client/src/lib/components/Input.svelte
@@ -49,6 +49,7 @@
 			{value}
 			name={id}
 			on:focus={onFocus}
+			step="0.01"
 			class={isErrors && !wasFocued ? 'input-error' : ''}
 		/>
 	{/if}
@@ -66,7 +67,8 @@
 	}
 
 	input,
-	select, textarea {
+	select,
+	textarea {
 		@apply appearance-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500;
 	}
 
