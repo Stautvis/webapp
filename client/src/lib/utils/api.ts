285449b84638b7ca051f0011e17826c40dbb786b
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-04 16:02:17 +0200

    Updated api util

--- client/src/lib/utils/api.ts
+++ client/src/lib/utils/api.ts
@@ -3,6 +3,14 @@ import { z, ZodError, type ZodIssue } from 'zod';
 
 const HOST = 'http://localhost:53124';
 
+type RequestReturn = string | Record<string, unknown> | unknown[] | undefined;
+type GetRequest = (url: string, cookies: Cookies | undefined) => Promise<RequestReturn>;
+type PostRequest = (
+	url: string,
+	body: Record<string, unknown> | null,
+	cookies: Cookies | undefined
+) => Promise<RequestReturn>;
+
 export class HttpRequestError extends Error {
 	constructor(public status: number, public message: string) {
 		super(message, { cause: status });
@@ -11,23 +19,18 @@ export class HttpRequestError extends Error {
 	}
 }
 
-export const post = async (
-	url: string,
-	body: Record<string, unknown> | null,
-	cookies: Cookies | undefined
-) => {
+const configureHeaders = (cookies: Cookies | undefined) => {
 	const Authorization = cookies === undefined ? '' : cookies.get('Bearer') || '';
-	const response = await fetch(HOST + url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization
-		},
-		body: JSON.stringify(body)
-	});
+
+	return {
+		'Content-Type': 'application/json',
+		Authorization
+	};
+};
+
+const response = async (response: Response) => {
 	const { status } = response;
 	const data = await returnData(response);
-	console.log(status);
 	if (isStatusOk(status)) return data;
 	else {
 		throwZodError(data, status);
@@ -37,8 +40,8 @@ export const post = async (
 const isStatusOk = (status: number) => status > 199 && status < 300;
 
 const returnData = async (response: Response) => {
-	const contentType = response.headers.get('Content-Type');
-	if (contentType?.includes('json')) {
+	const contentType = response.headers.get('Content-Type') || '';
+	if (contentType.includes('json')) {
 		return (await response.json()) as Record<string, unknown>;
 	}
 	return await response.text();
@@ -58,4 +61,23 @@ const throwZodError = (
 			message: data[key] as string
 		}));
 	throw new ZodError(errors);
+};
+
+// Methods ----------------------------------------------------------------------------------------
+
+export const get: GetRequest = async (url, cookies = undefined) => {
+	const res = await fetch(HOST + url, {
+		method: 'GET',
+		headers: configureHeaders(cookies)
+	});
+	return response(res);
+};
+
+export const post: PostRequest = async (url, body, cookies = undefined) => {
+	const res = await fetch(HOST + url, {
+		method: 'POST',
+		headers: configureHeaders(cookies),
+		body: JSON.stringify(body)
+	});
+	return response(res);
 };
