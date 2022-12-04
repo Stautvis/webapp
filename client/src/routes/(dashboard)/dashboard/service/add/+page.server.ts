d09a46b39b8968420a65b943d1ce3cc8dabd2638
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-04 15:05:04 +0200

    service add page

--- client/src/routes/(dashboard)/dashboard/service/add/+page.server.ts
+++ client/src/routes/(dashboard)/dashboard/service/add/+page.server.ts
@@ -1,20 +1,28 @@
-import { post } from '$lib/utils/api';
+import { HttpRequestError, post } from '$lib/utils/api';
 import { validateData } from '$lib/utils/validateData';
 import { invalid, redirect, type Actions } from '@sveltejs/kit';
 import { z, ZodError } from 'zod';
+import { zfd } from 'zod-form-data';
 
-export const serviceAddScheme = z.object({
-	title: z.string().min(1, { message: 'Title is required!' }),
-	description: z.string().min(1, { message: 'Description is required!' })
+const serviceAddScheme = zfd.formData({
+	title: zfd.text(z.string({ required_error: 'Title is required!' })),
+	description: zfd.text(z.string({ required_error: 'Description is required!' })),
+	price: zfd.numeric(
+		z
+			.number({ required_error: 'Price is required' })
+			.nonnegative({ message: 'Price must be greater than or equal to 0' })
+	)
 });
 
 export const actions: Actions = {
-	default: async ({ request, cookies, params }) => {
+	default: async ({ request, cookies }) => {
 		const { data, errors } = await validateData(request.formData(), serviceAddScheme);
+
+		// const company = cookies.get('Company');
 
 		if (Object.keys(errors).length > 0) return invalid(400, { data, errors });
 		try {
-			await post(`/company/${params.id}/service`, data, cookies);
+			await post(`/company/2/service`, data, cookies);
 		} catch (err) {
 			if (err instanceof ZodError) {
 				return invalid(400, {
@@ -22,7 +30,10 @@ export const actions: Actions = {
 					errors: err.flatten().fieldErrors as { [x: string]: string[] }
 				});
 			}
+			if (err instanceof HttpRequestError) {
+				if (err.status === 401) throw redirect(301, '/login');
+			}
 		}
-		throw redirect(301, '/');
+		throw redirect(301, '/dashboard/service');
 	}
 };
