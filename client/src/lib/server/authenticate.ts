94e1d0d2c0ee2076967174750a7d7d1494186199
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-17 20:07:20 +0200

    Added refresh token

--- client/src/lib/server/authenticate.ts
+++ client/src/lib/server/authenticate.ts
@@ -2,9 +2,7 @@ import api from '$lib/server/api';
 import type { Handle, RequestEvent } from '@sveltejs/kit';
 
 export const authenticate: Handle = async ({ event, resolve }) => {
-	const user = await getUer(event);
-
-	if (user === undefined) console.log('undefined user');
+	event.locals.user = await getUer(event);
 	const response = await resolve(event);
 	return response;
 };
@@ -12,11 +10,11 @@ export const authenticate: Handle = async ({ event, resolve }) => {
 type GetUser = (event: RequestEvent) => Promise<App.Locals['user'] | undefined>;
 const getUer: GetUser = async (event) => {
 	try {
-		const user = await api.get<App.IUser | undefined>(event, '/user');
-		console.log(user);
+		const user = await api.get<App.IUser>(event, '/user/logged');
+		const token = await api.post<string>(event, '/user/refreshToken', null);
+		event.cookies.set('auth', token);
 		return user;
 	} catch (err) {
-		console.log(err);
 		return undefined;
 	}
 };
