9455a70a11d900eb858ca14d0dafae9230ad42bc
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-17 20:11:01 +0200

    added basic components

---
+++ client/src/lib/components/Form/Form.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	type FormActions = 'post' | 'get';
+	export let method: FormActions = 'post';
+	export let action: HTMLFormElement['action'] = '';
+</script>
+
+<form {action} {method}>
+	<slot />
+</form>
