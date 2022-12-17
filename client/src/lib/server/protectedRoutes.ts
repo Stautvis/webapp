6a2a19e5a56b315f14d0af00211f5e0eaa1b27e0
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-17 20:07:31 +0200

    Added protected routes

---
+++ client/src/lib/server/protectedRoutes.ts
@@ -0,0 +1,41 @@
+import { redirect, type Handle } from '@sveltejs/kit';
+
+enum UserRoles {
+	ADMIN = 3,
+	OWNER = 2,
+	USER = 1
+}
+
+const PROTECTED_ROUTES: { [k: string]: UserRoles[] } = {
+	'/dashboard': [UserRoles.ADMIN, UserRoles.OWNER]
+};
+
+export const protectedRoutes: Handle = async ({ event, resolve }) => {
+	const { locals, url } = event;
+	const { user } = locals;
+	const { pathname } = url;
+
+	if (isProtected(pathname) && !hasUserRole(user, pathname)) {
+		throw redirect(303, `/login?redirect=${pathname}`);
+	}
+
+	const response = await resolve(event);
+	return response;
+};
+
+const hasUserRole = (user: App.Locals['user'], pathname: string) => {
+	const roles = getRoles(pathname);
+	return roles.some((role) => user?.roles.includes(role));
+};
+
+const isProtected = (pathname: string) => {
+	if (pathname === '/') return false;
+	const keys = Object.keys(PROTECTED_ROUTES);
+	return keys.some((key) => pathname.startsWith(key));
+};
+
+const getRoles = (pathname: string) => {
+	const key = Object.keys(PROTECTED_ROUTES).find((key) => key.startsWith(pathname));
+	if (key === undefined) return [];
+	return PROTECTED_ROUTES[key];
+};
