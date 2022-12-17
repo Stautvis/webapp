1c4fa1d54ccd6bbcb0fd5fd58d9156a16273ae3a
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-17 15:51:56 +0200

    back to old client

--- client-old/src/lib/components/Form.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	type FormActions = 'post' | 'get';
-	export let method: FormActions = 'post';
-	export let action: HTMLFormElement['action'] = '';
-</script>
-
-<form {action} {method}>
-	<slot />
-</form>
