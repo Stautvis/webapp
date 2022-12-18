769423c6fcca90ab5291c7c486ef816816fdd127
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-18 17:12:57 +0200

    Updated protected routes

--- client/src/lib/server/protectedRoutes.ts
+++ client/src/lib/server/protectedRoutes.ts
@@ -1,5 +1,5 @@
 import { HttpRequestError } from '$lib/server/api';
-import { redirect, type RequestEvent } from '@sveltejs/kit';
+import { type RequestEvent } from '@sveltejs/kit';
 
 enum UserRoles {
 	ADMIN = 3,
@@ -8,7 +8,8 @@ enum UserRoles {
 }
 
 const PROTECTED_ROUTES: { [k: string]: UserRoles[] } = {
-	'/dashboard': [UserRoles.ADMIN, UserRoles.OWNER]
+	'/dashboard': [UserRoles.ADMIN, UserRoles.OWNER],
+	'/settings': [UserRoles.USER]
 };
 
 export const protectedRoutes = async (event: RequestEvent) => {
@@ -16,8 +17,8 @@ export const protectedRoutes = async (event: RequestEvent) => {
 	const { pathname } = url;
 
 	if (isProtected(pathname)) {
-		if (event.locals.user === undefined) {
-			throw redirect(303, `/login?redirect=${pathname}`);
+		if (locals.user === undefined) {
+			throw new HttpRequestError(401, `/login?redirect=${pathname}`);
 		}
 		if (!hasUserRole(locals.user, pathname)) {
 			throw new HttpRequestError(403, 'Permission denied!');
@@ -27,31 +28,27 @@ export const protectedRoutes = async (event: RequestEvent) => {
 
 const hasUserRole = (user: App.Locals['user'], pathname: string) => {
 	const roles = getRoles(pathname);
-	console.log('USER ROLES');
+	if (roles.length === 0) return false;
 	const isAllowed = roles.some((role) => {
 		const include = user?.roles.includes(role);
-		console.log(role, include);
 		return include;
 	});
-	console.log(isAllowed);
 	return isAllowed;
 };
 
 const isProtected = (pathname: string) => {
 	if (pathname === '/') return false;
-	console.log('ROUTES');
 	const keys = Object.keys(PROTECTED_ROUTES);
 	const isAllowed = keys.some((key) => {
-		const include = pathname.includes(key);
-		console.log(include, pathname, key);
+		const regex = RegExp(key);
+		const include = regex.test(pathname);
 		return include;
 	});
-	console.log(isAllowed);
 	return isAllowed;
 };
 
 const getRoles = (pathname: string) => {
-	const key = Object.keys(PROTECTED_ROUTES).find((key) => key.startsWith(pathname));
+	const key = Object.keys(PROTECTED_ROUTES).find((key) => pathname.startsWith(key));
 	if (key === undefined) return [];
 	return PROTECTED_ROUTES[key];
 };
