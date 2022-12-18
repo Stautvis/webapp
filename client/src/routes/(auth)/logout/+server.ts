397c71a4a37a1e19d8185bc8df0bf08f3e81ede4
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-18 15:24:07 +0200

    changed cookies path

--- client/src/routes/(auth)/logout/+server.ts
+++ client/src/routes/(auth)/logout/+server.ts
@@ -1,8 +1,8 @@
 import { redirect, type RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ cookies }) => {
-	cookies.delete('auth');
-	cookies.delete('refreshToken');
-	// locals.user = undefined;
+export const GET: RequestHandler = async ({ cookies, locals }) => {
+	cookies.delete('auth', { path: '/' });
+	cookies.delete('refreshToken', { path: '/' });
+	locals.user = undefined;
 	throw redirect(301, '/');
 };
