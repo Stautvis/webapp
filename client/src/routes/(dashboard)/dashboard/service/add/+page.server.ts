e4ec421c278583c586c8db1879d9ebd6f300d89d
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-12-18 17:16:02 +0200

    service add

--- client/src/routes/(dashboard)/dashboard/service/add/+page.server.ts
+++ client/src/routes/(dashboard)/dashboard/service/add/+page.server.ts
@@ -1,4 +1,4 @@
-import { registerScheme } from '$lib/schemas/register';
+import { serviceAddScheme } from '$lib/schemas/serviceAdd';
 import api from '$lib/server/api';
 import { validateForm } from '$lib/server/validateForm';
 import { redirect, type Actions } from '@sveltejs/kit';
@@ -7,13 +7,15 @@ import { ZodError } from 'zod';
 export const actions: Actions = {
 	default: async (event) => {
 		const { request, url, locals } = event;
-		const { data, errors } = await validateForm(request.formData(), registerScheme);
+		const { data, errors } = await validateForm(request.formData(), serviceAddScheme);
 
 		if (Object.keys(errors).length > 0) return { data, errors };
 
 		try {
-			await api.post<string>(event, `/company/2/service`, data);
+			await api.post<string>(event, `/company/${locals.user?.companyID}/service`, data);
+			console.log('created');
 		} catch (err) {
+			console.log(err);
 			if (err instanceof ZodError) {
 				return {
 					data,
@@ -21,6 +23,6 @@ export const actions: Actions = {
 				};
 			}
 		}
-		throw redirect(301, url.searchParams.get('redirect') || '/');
+		throw redirect(301, url.searchParams.get('redirect') || '/dashboard/service');
 	}
 };
